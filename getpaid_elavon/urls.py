693aa596da20b7ccd8e4433365e8ac25913d6e8f
Adam Przyborowski <adam.przyborowski@ordergroup.pl>
2025-11-19 10:02:58 +0100

    DK-1235 init: og-django-getpaid-elavon

---
+++ getpaid_elavon/urls.py
@@ -0,0 +1,10 @@
+### Use this only if standard flow provided by django-getpaid does not cover your use-case.
+
+
+# from django.conf.urls import url
+#
+# from .views import ElavonReturnView
+#
+# urlpatterns = [
+#     url(r"^return/(?P<pk>[0-9a-f-]+)/$", ElavonReturnView.as_view(), name="return-view")
+# ]
---
+++ tests/test_processor.py
@@ -0,0 +1,20 @@
+"""
+test_og-django-getpaid-elavon
+------------
+
+Tests for `og-django-getpaid-elavon`.
+"""
+
+from django.test import TestCase
+
+
+class TestGetpaid_elavon(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
